828816d6b8fc6a18eae0b1b23fdb7809f99682e4
afik <k.afis3@rocketmail.com>
2015-04-05 21:49:27 +0700

    add some view

---
+++ resources/views/landing.php
@@ -0,0 +1,143 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Creation a industrial bootstrap Website Template | Home :: w3layouts</title>
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!--[if lt IE 9]>
+     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!--  webfonts  -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+<!-- // webfonts  -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- start plugins -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="header_bg"><!-- start header -->
+	<div class="container">
+		<div class="row header">
+		<nav class="navbar" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="landing.php"><img src="images/logo.png" alt="" class="img-responsive"/> </a>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="menu nav navbar-nav ">
+		        <li class="active"><a href="landing.php">beranda</a></li>
+		        <li><a href="feature.html">tentang kami</a></li>
+		        <li><a href="blog.html">login</a></li>
+		      </ul>
+		      <form class="navbar-form navbar-right" role="search">
+		        <div class="form-group my_search">
+		          <input type="text" class="form-control" placeholder="Search">
+		        </div>
+		        <button type="submit" class="btn btn-default btn-search">Search</button>
+		      </form>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+		</div>
+		<div class="row slider text-center">
+			<div class="col-md-8">
+					<div class="col-md-10 slider_text">
+						<h2>Dinas Pelayanan Pajak Kota Bandung</h2>
+						<h3>Ayo Bayar Pajak Tepat Waktu</h3>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="slider_img">
+						<img src="images/pic1.png" alt="" class="img-responsive"/>
+					</div>
+				</div>
+			</div>
+	</div>
+</div>
+<div class="main"><!-- start main -->
+<div class="container main">
+	<div class="row grids_of_3">
+				<div class="col-md-6 grid1_of_3">
+					  <h2>Tentang Pajak Bandung</h2>
+					  <img src="images/info.png" class="img-responsive"/>
+					  <p>Informasi lebih lanjut mengenai perpajakan di kota Bandung.</p>
+				     <div class="rd_more1">
+						<a href="single-page.html"><button class="btn_style">Baca</button></a>
+					</div>					
+				</div>
+				<div class="col-md-6 grid1_of_3">
+					<h2>Daftar NPWPD</h2>
+					  <img src="images/akun.png" class="img-responsive"/>
+					  <p>Daftarkan NPWPD untuk usaha anda dengan cepat dan mudah</p>
+				      <div class="rd_more1">
+						<a href="single-page.html"><button class="btn_style">daftar</button></a>
+					</div>					
+				</div>
+			    <div class="clearfix"></div>
+	</div>
+</div>
+</div>
+<div class="footer_bg"><!-- start footre -->
+	<div class="container">
+		<div class="row  footer">
+			<div class="col-md-6 span1_of_4">
+				<h4>Tentang Kami</h4>
+				<p>Dinas Pelayanan Pajak Kota Bandung</p>
+				<h5>Alamat</h5>
+				<p class="top">Jl. Wastukencana No.2,</p>
+				<p>Bandung,</p>
+				<p>Telepon:(022) 7215323</p>
+				<p>Twitter: @disyanjakkotbdg</p>
+			</div>
+			<div class="col-md-6 span1_of_4">
+				<h4>Informasi Terbaru</h4>
+				<span><a href="#"> Tata Cara Pembayaran Pajak </a></span>
+				<span><a href="#">Daftar Peraturan Daerah (Download)</a></span>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+<div class="footer_btm"><!-- start footer_btm -->
+	<div class="container">
+		<div class="row  footer1">
+			<div class="col-md-5">
+				<div class="soc_icons">
+					<ul class="list-unstyled">
+						<li><a class="icon1" href="#"></a></li>
+						<li><a class="icon2" href="#"></a></li>
+						<li><a class="icon3" href="#"></a></li>
+						<li><a class="icon4" href="#"></a></li>
+						<li><a class="icon5" href="#"></a></li>
+						<div class="clearfix"></div>
+					</ul>	
+				</div>
+			</div>
+			<div class="col-md-7 copy">
+				<p class="link text-right"><span>&#169; All rights reserved | Design by&nbsp;<a href="http://w3layouts.com/"> W3Layouts</a></span></p>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
