e946b29c3165fbe9ed9ca98b3150a43cf67f7234
winsxx <13512071@std.stei.itb.ac.id>
2015-04-07 15:45:53 +0700

    route postregister

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,7 @@ Route::get('admin/home', 'MainMenuController@getDinasHome');
 Route::get('login', 'Auth\AuthController@getLogin');
 Route::post('login', 'Auth\AuthController@postLogin');
 Route::get('register', 'Auth\AuthController@getRegister');
-Route::post('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
 Route::get('logout', 'Auth\AuthController@getLogout');
 
 /*
