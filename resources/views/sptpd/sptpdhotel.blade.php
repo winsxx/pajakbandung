3fc97abd83261f65934f547608c0be9683401620
afik <k.afis3@rocketmail.com>
2015-04-08 19:30:42 +0700

    add sptpd

---
+++ resources/views/sptpd/sptpdhotel.blade.php
@@ -0,0 +1,80 @@
+@extends('master')
+@section('title')
+	Mengajukan SPTPD Hotel
+@endsection
+@section('breadcrumb')
+<li><a href="/homewp"> Beranda </a></li>
+<li class="active"> SPTPD Hotel </li>
+@endsection
+@section('content')
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <div class="company_ad">
+        <h2> Pengajuan SPTPD </h2>
+    </div>
+    <form class="form-horizontal" role="form" method="POST" action="">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="form-group">
+            <label class="col-md-4 control-label">Pajak yang dilaporkan</label>
+        </div>
+
+        <div class="form-group">
+            <label class="col-md-4 control-label">Bulan</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" name="bulan-sptpd" value="">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-md-4 control-label">Penjualan kamar</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" name="penjualan-kamar" value="">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-md-4 control-label">Penjualan makanan</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" name="penjualan-makanan" value="">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-md-4 control-label">Laundry</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" name="laundry" value="">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-md-4 control-label">Sewa Ruangan</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" name="sewa-ruangan" value="">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-4 control-label">Service</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" name="service" value="">
+            </div>
+        </div>
+
+        <div class="form-group" style="margin-top:10%;">
+            <div class="col-md-6 col-md-offset-8">
+                <button type="submit" class="btn btn-primary">
+                    Submit
+                </button>
+            </div>
+        </div>
+    </form>
+@endsection
