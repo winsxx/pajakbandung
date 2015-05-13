810b616aa55657f8b89bee8eb6e63f4b088c3b77
winsxx <13512071@std.stei.itb.ac.id>
2015-05-14 01:01:16 +0700

    Edit sso

--- app/Http/Controllers/MainMenuController.php
+++ app/Http/Controllers/MainMenuController.php
@@ -23,8 +23,7 @@ class MainMenuController extends Controller {
 
     public function getIndex(Request $request){
         if($request->id != null){
-            Auth::loginUsingId( intval($request->id) );
-            //Auth::attempt(['nik'=>Penduduk::find($request->$id)->nik, 'pass'=>Penduduk::find($request->$id)->password]);
+            Auth::attempt(['nik'=>Penduduk::find($request->$id)->nik, 'pass'=>Penduduk::find($request->$id)->password]);
         }
         return view('mainmenu.landing');
     }
